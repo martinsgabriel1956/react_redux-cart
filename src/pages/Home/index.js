5b700bc334087151dfa9ec7ae369e8aed5db6145
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-12 18:00:00 -0300

    feat: using useEffect() with redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+
 import { Cart } from '../../components/Cart';
 import { Layout } from '../../components/Layout';
 import { Products } from '../../components/Shop/Products';
 
 export function Home() {
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-redux-cart-20c97-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+
+  }, [cart]);
 
   return (
     <Layout>
