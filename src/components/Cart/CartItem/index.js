fe3c7d6298fe1fc9087c59da464b21b49b9c0977
Gabriel Martins <martinsgabriel1956@gmail.com>
2021-07-12 17:53:18 -0300

    feat: where to put our logic

--- src/components/Cart/CartItem/index.js
+++ src/components/Cart/CartItem/index.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch} from 'react-redux';
 
 import { Container, Details, Price, Quantity, Actions } from './styles';
 
--- src/components/Shop/ProductItem/index.js
+++ src/components/Shop/ProductItem/index.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Container, Price, Actions } from "./styles";
 
@@ -6,20 +6,42 @@ import { Card } from "../../UI/Card";
 import { cartActions } from '../../../store/cart';
 
 export function ProductItem(props) {
+  const cart = useSelector((state) => state.cart);
+  
   const { title, price, description, id} = props;
 
   const dispatch = useDispatch();
 
   function handleAddToCard() {
-    dispatch(
-      cartActions.addItem({
-        id,
-        title,
-        price,
-      })
-    );    
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice(); // create copy via slice to avoid mutating original state
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; // new object + copy existing properties to avoid state mutation
+      updatedItem.quantity++;
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
+        id: id,
+        price: price,
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
   };
 
+  const newCart = {
+    totalQuantity: newTotalQuantity,
+    items: updatedItems,
+  };
+
+  dispatch(cartActions.replaceCart(newCart));
+}
   return (
     <Container>
       <Card>
